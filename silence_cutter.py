8ed85faf52e49ec6132d7b14f92a6bbf4923e326
Redblue232 <romain.benoit05@gmail.com>
2024-07-22 15:41:57 +0200

    Edit that cut only 15seconds and more silences

--- silence_cutter.py
+++ silence_cutter.py
@@ -1,198 +1,77 @@
-# Created by DarkTrick - 4fb5f723849d32782e723c34bfd132e442d378d7
-
+import argparse
 import subprocess
-import tempfile
-import sys
-import os
-import logging
+import re
 
-# ===========================
-# ==== Configure logging ====
-# ===========================
-log_level = logging.ERROR
-log_filename = 'silence_cutter.log'
-logger = logging.getLogger('')
-logger.setLevel(log_level)
-log_handler = logging.FileHandler(log_filename, delay=True)
-logger.addHandler(log_handler)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Cut silences from a video.')
+    parser.add_argument('infile', type=str, help='Input video file')
+    parser.add_argument('outfile', type=str, help='Output video file')
+    parser.add_argument('silence_dB', type=float, help='Silence dB threshold')
+    parser.add_argument('min_silence_duration', type=int, help='Minimum silence duration to cut in seconds')
+    return parser.parse_args()
 
+def get_video_duration(infile):
+    command = [
+        'ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', infile
+    ]
+    result = subprocess.run(command, stdout=subprocess.PIPE, universal_newlines=True)
+    return float(result.stdout.strip())
 
-def findSilences(filename, dB = -35):
-  """
-    returns a list:
-      even elements (0,2,4, ...) denote silence start time
-      uneven elements (1,3,5, ...) denote silence end time
+def findSilences(infile, silence_dB, min_silence_duration):
+    command = [
+        'ffmpeg', '-i', infile, '-af',
+        f'silencedetect=noise={silence_dB}dB:d={min_silence_duration}', 
+        '-f', 'null', '-'
+    ]
+    result = subprocess.run(command, stderr=subprocess.PIPE, universal_newlines=True)
+    output = result.stderr
 
-  """
-  logging.debug(f"findSilences ()")
-  logging.debug(f"    - filename = {filename}")
-  logging.debug(f"    - dB = {dB}")
+    silence_starts = []
+    silence_ends = []
 
-  command = ["ffmpeg","-i",filename,
-             "-af","silencedetect=n=" + str (dB) + "dB:d=1",
-             "-f","null","-"]
-  output = subprocess.run (command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  s = str(output)
-  lines = s.split("\\n")
-  time_list = []
-  logging.debug("  lines: ```\n" + "\n".join(lines) + "```\n\n")
+    for line in output.split('\n'):
+        if 'silence_start' in line:
+            silence_start = float(re.search(r'silence_start: (\d+(\.\d+)?)', line).group(1))
+            silence_starts.append(silence_start)
+        elif 'silence_end' in line:
+            silence_end = float(re.search(r'silence_end: (\d+(\.\d+)?)', line).group(1))
+            silence_ends.append(silence_end)
 
-  for line in lines:
-    if ("silencedetect" in line):
-        words = line.split(" ")
-        logging.debug("  words: " + str(words))
-        for i in range (len(words)):
-            if ("silence_start" in words[i]):
-              time_list.append (float(words[i+1]))
-            if "silence_end" in words[i]:
-              time_list.append (float (words[i+1]))
-  silence_section_list = list (zip(*[iter(time_list)]*2))
+    return list(zip(silence_starts, silence_ends))
 
-  #return silence_section_list
-  return time_list
+def cut_silences(infile, outfile, silence_dB, min_silence_duration):
+    silences = findSilences(infile, silence_dB, min_silence_duration)
+    duration = get_video_duration(infile)
 
+    segments = []
+    last_end = 0
+    for start, end in silences:
+        if start > last_end:
+            segments.append((last_end, start))
+        last_end = end
+    if last_end < duration:
+        segments.append((last_end, duration))
 
+    filters = []
+    for i, (start, end) in enumerate(segments):
+        filters.append(f"[0:v]trim=start={start}:end={end},setpts=PTS-STARTPTS[v{i}];")
+        filters.append(f"[0:a]atrim=start={start}:end={end},asetpts=PTS-STARTPTS[a{i}];")
 
-def getVideoDuration(filename:str) -> float:
-  logging.debug(f"getVideoDuration ()")
-  logging.debug(f"    - filename = {filename}")
+    filter_complex = ''.join(filters)
+    concat_filter = ''.join([f"[v{i}][a{i}]" for i in range(len(segments))])
+    concat_filter += f"concat=n={len(segments)}:v=1:a=1[v][a]"
 
-  command = ["ffprobe","-i",filename,"-v","quiet",
-             "-show_entries","format=duration","-hide_banner",
-             "-of","default=noprint_wrappers=1:nokey=1"]
+    command = [
+        'ffmpeg', '-hwaccel', 'cuda', '-hwaccel_output_format', 'cuda', '-i', infile, 
+        '-filter_complex', filter_complex + concat_filter,
+        '-map', '[v]', '-map', '[a]', '-c:v', 'h264_nvenc', outfile
+    ]
 
-  output = subprocess.run (command, stdout=subprocess.PIPE)
-  s = str(output.stdout, "UTF-8")
-  return float (s)
-
-def getSectionsOfNewVideo (silences, duration):
-  """Returns timings for parts, where the video should be kept"""
-  return [0.0] + silences + [duration]
-
-
-def ffmpeg_filter_getSegmentFilter(videoSectionTimings):
-  ret = ""
-  for i in range (int (len(videoSectionTimings)/2)):
-    start = videoSectionTimings[2*i]
-    end   = videoSectionTimings[2*i+1]
-    ret += "between(t," + str(start) + "," + str(end) + ")+"
-  # cut away last "+"
-  ret = ret[:-1]
-  return ret
-
-def getFileContent_videoFilter(videoSectionTimings):
-  ret = "select='"
-  ret += ffmpeg_filter_getSegmentFilter (videoSectionTimings)
-  ret += "', setpts=N/FRAME_RATE/TB"
-  return ret
-
-def getFileContent_audioFilter(videoSectionTimings):
-  ret = "aselect='"
-  ret += ffmpeg_filter_getSegmentFilter (videoSectionTimings)
-  ret += "', asetpts=N/SR/TB"
-  return ret
-
-def writeFile (filename, content):
-  logging.debug(f"writeFile ()")
-  logging.debug(f"    - filename = {filename}")
-
-  with open (filename, "w") as file:
-    file.write (str(content))
-
-
-def ffmpeg_run (file, videoFilter, audioFilter, outfile):
-  logging.debug(f"ffmpeg_run ()")
-
-  # prepare filter files
-  vFile = tempfile.NamedTemporaryFile (mode="w", encoding="UTF-8", prefix="silence_video")
-  aFile = tempfile.NamedTemporaryFile (mode="w", encoding="UTF-8", prefix="silence_audio")
-
-  videoFilter_file = vFile.name #"/tmp/videoFilter" # TODO: replace with tempfile
-  audioFilter_file = aFile.name #"/tmp/audioFilter" # TODO: replace with tempfile
-  writeFile (videoFilter_file, videoFilter)
-  writeFile (audioFilter_file, audioFilter)
-
-  command = ["ffmpeg","-i",file,
-              "-filter_script:v",videoFilter_file,
-              "-filter_script:a",audioFilter_file,
-              outfile]
-  subprocess.run (command)
-
-  vFile.close()
-  aFile.close()
-
-
-
-def cut_silences(infile, outfile, dB = -35):
-  logging.debug(f"cut_silences ()")
-  logging.debug(f"    - infile = {infile}")
-  logging.debug(f"    - outfile = {outfile}")
-  logging.debug(f"    - dB = {dB}")
-
-  print ("detecting silences")
-  silences = findSilences (infile,dB)
-  duration = getVideoDuration (infile)
-  videoSegments = getSectionsOfNewVideo (silences, duration)
-
-  videoFilter = getFileContent_videoFilter (videoSegments)
-  audioFilter = getFileContent_audioFilter (videoSegments)
-
-  print ("create new video")
-  ffmpeg_run (infile, videoFilter, audioFilter, outfile)
-
-def printHelp():
-  print ("Usage:")
-  print ("   silence_cutter.py [infile] [optional: outfile] [optional: dB]")
-  print ("   ")
-  print ("        [outfile]")
-  print ("         Default: [infile]_cut")
-  print ("   ")
-  print ("        [dB]")
-  print ("         Default: -30")
-  print ("         A suitable value might be around -50 to -35.")
-  print ("         The lower the more volume will be defined das 'silent'")
-  print ("         -30: Cut Mouse clicks and mouse movent; cuts are very recognizable.")
-  print ("         -35: Cut inhaling breath before speaking; cuts are quite recognizable.")
-  print ("         -40: Cuts are almost not recognizable.")
-  print ("         -50: Cuts are almost not recognizable.")
-  print ("              Cuts nothing, if there is background noise.")
-  print ("         ")
-  print ("")
-  print ("Dependencies:")
-  print ("          ffmpeg")
-  print ("          ffprobe")
+    subprocess.run(command)
 
 def main():
-  logging.debug(f"main ()")
-  args = sys.argv[1:]
-  if (len(args) < 1):
-    printHelp()
-    return
-
-  if (args[0] == "--help"):
-    printHelp()
-    return
-
-  infile = args[0]
-
-  if (not os.path.isfile (infile)):
-    print ("ERROR: The infile could not be found:\n" + infile)
-    return
-
-  # set default values for optionl arguments
-  tmp = os.path.splitext (infile)
-  outfile = tmp[0] + "_cut" + tmp[1]
-  dB = -30
-
-  if (len(args) >= 2):
-    outfile = args[1]
-
-  if (len(args) >= 3):
-    dB = args[2]
-
-
-  cut_silences (infile, outfile, dB)
-
+    args = parse_arguments()
+    cut_silences(args.infile, args.outfile, args.silence_dB, args.min_silence_duration)
 
 if __name__ == "__main__":
-  main()
+    main()
